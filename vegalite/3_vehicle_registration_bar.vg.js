d8a13d66052c51d417060c4791620bd25adff26c
Loh Xiao Ying <xloh0003@student.monash.edu>
2026-05-31 13:57:58 +0800

    Upload chart files

---
+++ vegalite/3_vehicle_registration_bar.vg.js
@@ -0,0 +1,117 @@
+{
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json", 
+  "width": 680,
+  "height": 420,
+  "data": {
+    "url": "https://raw.githubusercontent.com//xloh0003-a11y/2179/main/data/registrations_type_fuel.csv",
+    "format": {"type": "csv"}
+  },
+  "params": [
+    {
+      "name": "typeSelect",
+      "select": {"type": "point", "fields": ["type"]}
+    }
+  ],
+  "transform": [
+    {
+      "filter": "datum.fuel == 'all_fuels'"
+    },
+    {
+      "filter": "datum.type != 'all_types'"
+    },
+    {
+      "calculate": "year(toDate(datum.date))",
+      "as": "year"
+    },
+    {
+      "filter": "datum.year >= 2021 && datum.year <= 2025"
+    },
+    {
+      "calculate": "toString(datum.year)",
+      "as": "year_str"
+    }
+  ],
+  "encoding": {
+    "x": {
+      "field": "year_str",
+      "type": "nominal",
+      "title": "Year",
+      "axis": {
+        "labelFontSize": 12,
+        "titleFontSize": 13,
+        "labelAngle": 0
+      },
+      "sort": ["2021", "2022", "2023", "2024", "2025"]
+    },
+    "xOffset": {
+      "field": "type",
+      "type": "nominal",
+      "sort": ["car", "motorcycle", "van", "lorry", "bus", "other"],
+      "scale": {"paddingInner": 0.15}
+    },
+    "y": {
+      "aggregate": "sum",
+      "field": "registrations",
+      "type": "quantitative",
+      "title": "Total Registrations",
+      "axis": {
+        "labelFontSize": 11,
+        "titleFontSize": 13,
+        "format": "~s",
+        "grid": true,
+        "gridDash": [3, 3],
+        "gridOpacity": 0.5
+      }
+    },
+    "color": {
+      "field": "type",
+      "type": "nominal",
+      "title": "Vehicle Type",
+      "sort": ["car", "motorcycle", "van", "lorry", "bus", "other"],
+      "scale": {
+          "domain": ["car", "motorcycle", "van", "lorry", "bus", "other"],
+          "range": ["#1f77b4", "#ff7f0e", "#2ca02c", "#9467bd", "#17becf", "#bcbd22"]
+        },
+      "legend": {
+        "titleFontSize": 12,
+        "labelFontSize": 11,
+        "orient": "right",
+        "symbolSize": 120
+      }
+    },
+    "opacity": {
+      "condition": {
+        "param": "typeSelect",
+        "value": 1.0
+      },
+      "value": 0.2
+    },
+    "tooltip": [
+      {"field": "year_str", "type": "nominal", "title": "Year"},
+      {"field": "type", "type": "nominal", "title": "Vehicle Type"},
+      {
+        "aggregate": "sum",
+        "field": "registrations",
+        "type": "quantitative",
+        "title": "Total Registrations",
+        "format": ","
+      }
+    ]
+  },
+  "mark": {
+    "type": "bar",
+    "cursor": "pointer",
+    "cornerRadiusTopLeft": 3,
+    "cornerRadiusTopRight": 3
+  },
+  "config": {
+    "view": {"stroke": "transparent"},
+    "axis": {"domainColor": "#aaa", "tickColor": "#aaa"},
+    "scale": {
+      "bandPaddingInner": 0.25,
+      "bandPaddingOuter": 0.15
+    },
+    "background": "#fafafa",
+    "font": "sans-serif"
+  }
+}
---
+++ vegalite/7_daily_transit_heatmap.vg.js
@@ -0,0 +1,747 @@
+{
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "width": {
+    "step": 62
+  },
+  "height": {
+    "step": 56
+  },
+  "data": {
+    "values": [
+      {
+        "date": "2026-01-01",
+        "ridership": 1487166
+      },
+      {
+        "date": "2026-01-02",
+        "ridership": 1788966
+      },
+      {
+        "date": "2026-01-03",
+        "ridership": 1368720
+      },
+      {
+        "date": "2026-01-04",
+        "ridership": 1183758
+      },
+      {
+        "date": "2026-01-05",
+        "ridership": 1986342
+      },
+      {
+        "date": "2026-01-06",
+        "ridership": 2070105
+      },
+      {
+        "date": "2026-01-07",
+        "ridership": 2105295
+      },
+      {
+        "date": "2026-01-08",
+        "ridership": 2031309
+      },
+      {
+        "date": "2026-01-09",
+        "ridership": 2063256
+      },
+      {
+        "date": "2026-01-10",
+        "ridership": 1588422
+      },
+      {
+        "date": "2026-01-11",
+        "ridership": 1227888
+      },
+      {
+        "date": "2026-01-12",
+        "ridership": 1971789
+      },
+      {
+        "date": "2026-01-13",
+        "ridership": 2093475
+      },
+      {
+        "date": "2026-01-14",
+        "ridership": 2125938
+      },
+      {
+        "date": "2026-01-15",
+        "ridership": 2084811
+      },
+      {
+        "date": "2026-01-16",
+        "ridership": 2150514
+      },
+      {
+        "date": "2026-01-17",
+        "ridership": 1436520
+      },
+      {
+        "date": "2026-01-18",
+        "ridership": 1225662
+      },
+      {
+        "date": "2026-01-19",
+        "ridership": 2029986
+      },
+      {
+        "date": "2026-01-20",
+        "ridership": 2106801
+      },
+      {
+        "date": "2026-01-21",
+        "ridership": 2124384
+      },
+      {
+        "date": "2026-01-22",
+        "ridership": 2097570
+      },
+      {
+        "date": "2026-01-23",
+        "ridership": 2138679
+      },
+      {
+        "date": "2026-01-24",
+        "ridership": 1429482
+      },
+      {
+        "date": "2026-01-25",
+        "ridership": 1248606
+      },
+      {
+        "date": "2026-01-26",
+        "ridership": 2051691
+      },
+      {
+        "date": "2026-01-27",
+        "ridership": 2129838
+      },
+      {
+        "date": "2026-01-28",
+        "ridership": 2143914
+      },
+      {
+        "date": "2026-01-29",
+        "ridership": 2120571
+      },
+      {
+        "date": "2026-01-30",
+        "ridership": 2274054
+      },
+      {
+        "date": "2026-01-31",
+        "ridership": 1715850
+      },
+      {
+        "date": "2026-02-01",
+        "ridership": 1495548
+      },
+      {
+        "date": "2026-02-02",
+        "ridership": 1188162
+      },
+      {
+        "date": "2026-02-03",
+        "ridership": 1470636
+      },
+      {
+        "date": "2026-02-04",
+        "ridership": 2157432
+      },
+      {
+        "date": "2026-02-05",
+        "ridership": 2199939
+      },
+      {
+        "date": "2026-02-06",
+        "ridership": 2262030
+      },
+      {
+        "date": "2026-02-07",
+        "ridership": 1716477
+      },
+      {
+        "date": "2026-02-08",
+        "ridership": 1443819
+      },
+      {
+        "date": "2026-02-09",
+        "ridership": 2203026
+      },
+      {
+        "date": "2026-02-10",
+        "ridership": 2291838
+      },
+      {
+        "date": "2026-02-11",
+        "ridership": 2316999
+      },
+      {
+        "date": "2026-02-12",
+        "ridership": 2306097
+      },
+      {
+        "date": "2026-02-13",
+        "ridership": 2338671
+      },
+      {
+        "date": "2026-02-14",
+        "ridership": 1698366
+      },
+      {
+        "date": "2026-02-15",
+        "ridership": 1399911
+      },
+      {
+        "date": "2026-02-16",
+        "ridership": 1719030
+      },
+      {
+        "date": "2026-02-17",
+        "ridership": 1352766
+      },
+      {
+        "date": "2026-02-18",
+        "ridership": 1187154
+      },
+      {
+        "date": "2026-02-19",
+        "ridership": 1343679
+      },
+      {
+        "date": "2026-02-20",
+        "ridership": 1472964
+      },
+      {
+        "date": "2026-02-21",
+        "ridership": 1018608
+      },
+      {
+        "date": "2026-02-22",
+        "ridership": 940824
+      },
+      {
+        "date": "2026-02-23",
+        "ridership": 1934823
+      },
+      {
+        "date": "2026-02-24",
+        "ridership": 1983555
+      },
+      {
+        "date": "2026-02-25",
+        "ridership": 1997817
+      },
+      {
+        "date": "2026-02-26",
+        "ridership": 2055858
+      },
+      {
+        "date": "2026-02-27",
+        "ridership": 2103921
+      },
+      {
+        "date": "2026-02-28",
+        "ridership": 1332510
+      },
+      {
+        "date": "2026-03-01",
+        "ridership": 1155873
+      },
+      {
+        "date": "2026-03-02",
+        "ridership": 2052978
+      },
+      {
+        "date": "2026-03-03",
+        "ridership": 2152080
+      },
+      {
+        "date": "2026-03-04",
+        "ridership": 2159046
+      },
+      {
+        "date": "2026-03-05",
+        "ridership": 2158668
+      },
+      {
+        "date": "2026-03-06",
+        "ridership": 2163576
+      },
+      {
+        "date": "2026-03-07",
+        "ridership": 1432995
+      },
+      {
+        "date": "2026-03-08",
+        "ridership": 1264620
+      },
+      {
+        "date": "2026-03-09",
+        "ridership": 2087538
+      },
+      {
+        "date": "2026-03-10",
+        "ridership": 2182527
+      },
+      {
+        "date": "2026-03-11",
+        "ridership": 2058918
+      },
+      {
+        "date": "2026-03-12",
+        "ridership": 2169768
+      },
+      {
+        "date": "2026-03-13",
+        "ridership": 2193861
+      },
+      {
+        "date": "2026-03-14",
+        "ridership": 1473615
+      },
+      {
+        "date": "2026-03-15",
+        "ridership": 1256952
+      },
+      {
+        "date": "2026-03-16",
+        "ridership": 2094126
+      },
+      {
+        "date": "2026-03-17",
+        "ridership": 2189472
+      },
+      {
+        "date": "2026-03-18",
+        "ridership": 2074857
+      },
+      {
+        "date": "2026-03-19",
+        "ridership": 1735092
+      },
+      {
+        "date": "2026-03-20",
+        "ridership": 1185174
+      },
+      {
+        "date": "2026-03-21",
+        "ridership": 1365747
+      },
+      {
+        "date": "2026-03-22",
+        "ridership": 1382610
+      },
+      {
+        "date": "2026-03-23",
+        "ridership": 1126683
+      },
+      {
+        "date": "2026-03-24",
+        "ridership": 1591851
+      },
+      {
+        "date": "2026-03-25",
+        "ridership": 1744698
+      },
+      {
+        "date": "2026-03-26",
+        "ridership": 1847346
+      },
+      {
+        "date": "2026-03-27",
+        "ridership": 1843053
+      },
+      {
+        "date": "2026-03-28",
+        "ridership": 1227534
+      },
+      {
+        "date": "2026-03-29",
+        "ridership": 1104960
+      },
+      {
+        "date": "2026-03-30",
+        "ridership": 1977654
+      },
+      {
+        "date": "2026-03-31",
+        "ridership": 2105670
+      },
+      {
+        "date": "2026-04-01",
+        "ridership": 2132328
+      },
+      {
+        "date": "2026-04-02",
+        "ridership": 2114514
+      },
+      {
+        "date": "2026-04-03",
+        "ridership": 2137230
+      },
+      {
+        "date": "2026-04-04",
+        "ridership": 1441551
+      },
+      {
+        "date": "2026-04-05",
+        "ridership": 1260129
+      },
+      {
+        "date": "2026-04-06",
+        "ridership": 2022900
+      },
+      {
+        "date": "2026-04-07",
+        "ridership": 2137227
+      },
+      {
+        "date": "2026-04-08",
+        "ridership": 2166138
+      },
+      {
+        "date": "2026-04-09",
+        "ridership": 2137230
+      },
+      {
+        "date": "2026-04-10",
+        "ridership": 2127654
+      },
+      {
+        "date": "2026-04-11",
+        "ridership": 1467804
+      },
+      {
+        "date": "2026-04-12",
+        "ridership": 1282818
+      },
+      {
+        "date": "2026-04-13",
+        "ridership": 2037234
+      },
+      {
+        "date": "2026-04-14",
+        "ridership": 2105247
+      },
+      {
+        "date": "2026-04-15",
+        "ridership": 2153862
+      },
+      {
+        "date": "2026-04-16",
+        "ridership": 2119638
+      },
+      {
+        "date": "2026-04-17",
+        "ridership": 2161842
+      },
+      {
+        "date": "2026-04-18",
+        "ridership": 1502472
+      },
+      {
+        "date": "2026-04-19",
+        "ridership": 1247889
+      },
+      {
+        "date": "2026-04-20",
+        "ridership": 2038230
+      },
+      {
+        "date": "2026-04-21",
+        "ridership": 2080950
+      },
+      {
+        "date": "2026-04-22",
+        "ridership": 2118714
+      },
+      {
+        "date": "2026-04-23",
+        "ridership": 2076288
+      },
+      {
+        "date": "2026-04-24",
+        "ridership": 2105325
+      },
+      {
+        "date": "2026-04-25",
+        "ridership": 1453737
+      },
+      {
+        "date": "2026-04-26",
+        "ridership": 1290243
+      },
+      {
+        "date": "2026-04-27",
+        "ridership": 1993014
+      },
+      {
+        "date": "2026-04-28",
+        "ridership": 2064924
+      },
+      {
+        "date": "2026-04-29",
+        "ridership": 2132106
+      },
+      {
+        "date": "2026-04-30",
+        "ridership": 2216232
+      },
+      {
+        "date": "2026-05-01",
+        "ridership": 1626054
+      },
+      {
+        "date": "2026-05-02",
+        "ridership": 1592940
+      },
+      {
+        "date": "2026-05-03",
+        "ridership": 1235106
+      },
+      {
+        "date": "2026-05-04",
+        "ridership": 2018031
+      },
+      {
+        "date": "2026-05-05",
+        "ridership": 2093289
+      },
+      {
+        "date": "2026-05-06",
+        "ridership": 2136783
+      },
+      {
+        "date": "2026-05-07",
+        "ridership": 2111955
+      },
+      {
+        "date": "2026-05-08",
+        "ridership": 2117928
+      },
+      {
+        "date": "2026-05-09",
+        "ridership": 1446459
+      },
+      {
+        "date": "2026-05-10",
+        "ridership": 1290651
+      },
+      {
+        "date": "2026-05-11",
+        "ridership": 2063688
+      },
+      {
+        "date": "2026-05-12",
+        "ridership": 2127870
+      },
+      {
+        "date": "2026-05-13",
+        "ridership": 2123805
+      },
+      {
+        "date": "2026-05-14",
+        "ridership": 2105550
+      },
+      {
+        "date": "2026-05-15",
+        "ridership": 2104128
+      },
+      {
+        "date": "2026-05-16",
+        "ridership": 1406466
+      },
+      {
+        "date": "2026-05-17",
+        "ridership": 1204566
+      },
+      {
+        "date": "2026-05-18",
+        "ridership": 1924701
+      },
+      {
+        "date": "2026-05-19",
+        "ridership": 2004234
+      },
+      {
+        "date": "2026-05-20",
+        "ridership": 2058324
+      },
+      {
+        "date": "2026-05-21",
+        "ridership": 2022408
+      },
+      {
+        "date": "2026-05-22",
+        "ridership": 2068344
+      },
+      {
+        "date": "2026-05-23",
+        "ridership": 1429272
+      },
+      {
+        "date": "2026-05-24",
+        "ridership": 1267995
+      },
+      {
+        "date": "2026-05-25",
+        "ridership": 1917819
+      },
+      {
+        "date": "2026-05-26",
+        "ridership": 1889553
+      },
+      {
+        "date": "2026-05-27",
+        "ridership": 1237485
+      },
+      {
+        "date": "2026-05-28",
+        "ridership": 1664094
+      }
+    ]
+  },
+  "transform": [
+    {
+      "calculate": "toDate(datum.date)",
+      "as": "parsed_date"
+    },
+    {
+      "calculate": "day(datum.parsed_date)",
+      "as": "day_num"
+    },
+    {
+      "calculate": "month(datum.parsed_date)",
+      "as": "month_num"
+    },
+    {
+      "calculate": "['Sun','Mon','Tue','Wed','Thu','Fri','Sat'][datum.day_num]",
+      "as": "day_label"
+    },
+    {
+      "calculate": "['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'][datum.month_num]",
+      "as": "month_label"
+    },
+    {
+      "calculate": "datum.month_num * 10 + datum.day_num",
+      "as": "sort_key"
+    }
+  ],
+  "mark": {
+    "type": "rect",
+    "stroke": "white",
+    "strokeWidth": 1.5,
+    "cornerRadius": 2
+  },
+  "encoding": {
+    "x": {
+      "field": "day_label",
+      "type": "nominal",
+      "title": null,
+      "sort": [
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+        "Sun"
+      ],
+      "axis": {
+        "labelFontSize": 12,
+        "labelFontWeight": "bold",
+        "labelColor": "#444",
+        "labelPadding": 8,
+        "ticks": false,
+        "domain": false,
+        "orient": "top"
+      }
+    },
+    "y": {
+      "field": "month_label",
+      "type": "nominal",
+      "title": null,
+      "sort": [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ],
+      "axis": {
+        "labelFontSize": 12,
+        "labelFontWeight": "bold",
+        "labelColor": "#444",
+        "labelPadding": 10,
+        "ticks": false,
+        "domain": false
+      }
+    },
+    "color": {
+      "field": "ridership",
+      "type": "quantitative",
+      "title": "Total Passengers",
+      "aggregate": "sum",
+      "scale": {
+        "scheme": "magma",
+        "reverse": false
+      },
+      "legend": {
+        "orient": "right",
+        "titleFontSize": 11,
+        "labelFontSize": 10,
+        "gradientLength": 160,
+        "gradientThickness": 12,
+        "titlePadding": 6,
+        "labelOffset": 4,
+        "format": "~s"
+      }
+    },
+    "tooltip": [
+      {
+        "field": "date",
+        "type": "nominal",
+        "title": "Date"
+      },
+      {
+        "field": "day_label",
+        "type": "nominal",
+        "title": "Day"
+      },
+      {
+        "field": "month_label",
+        "type": "nominal",
+        "title": "Month"
+      },
+      {
+        "field": "ridership",
+        "type": "quantitative",
+        "title": "Total Passengers",
+        "format": ","
+      }
+    ]
+  },
+  "config": {
+    "view": {
+      "stroke": "transparent"
+    },
+    "background": "#fafafa",
+    "font": "sans-serif",
+    "scale": {
+      "bandPaddingInner": 0.08,
+      "bandPaddingOuter": 0.1
+    },
+    "axis": {
+      "labelFont": "sans-serif",
+      "grid": false
+    }
+  }
+}
